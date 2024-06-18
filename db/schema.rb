8f63390f0566391118d77aa3600d071b0c37aa82
Luigi.M292 <luigimaretto292@gmail.com>
2024-06-18 14:11:27 +0100

    App Ready to be Developed

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,119 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_13_140908) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_29_131941) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "applications", force: :cascade do |t|
+    t.string "status"
+    t.binary "cv_file"
+    t.binary "cover_letter"
+    t.bigint "user_id", null: false
+    t.bigint "job_id", null: false
+    t.bigint "resume_id"
+    t.bigint "cover_letter_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cover_letter_id"], name: "index_applications_on_cover_letter_id"
+    t.index ["job_id"], name: "index_applications_on_job_id"
+    t.index ["resume_id"], name: "index_applications_on_resume_id"
+    t.index ["user_id"], name: "index_applications_on_user_id"
+  end
+
+  create_table "cover_letters", force: :cascade do |t|
+    t.binary "file"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cover_letters_on_user_id"
+  end
+
+  create_table "experiences", force: :cascade do |t|
+    t.string "title"
+    t.string "company_name"
+    t.text "description"
+    t.string "location"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_experiences_on_user_id"
+  end
+
+  create_table "job_skills", force: :cascade do |t|
+    t.bigint "skill_id", null: false
+    t.bigint "job_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["job_id"], name: "index_job_skills_on_job_id"
+    t.index ["skill_id"], name: "index_job_skills_on_skill_id"
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "company_name"
+    t.text "requirements"
+    t.string "location"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
+    t.index ["user_id"], name: "index_jobs_on_user_id"
+  end
+
+  create_table "resumes", force: :cascade do |t|
+    t.binary "file"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_resumes_on_user_id"
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_skills_on_user_id"
+  end
+
+  create_table "user_skills", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "skill_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["skill_id"], name: "index_user_skills_on_skill_id"
+    t.index ["user_id"], name: "index_user_skills_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,8 +132,37 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_13_140908) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "business"
+    t.string "first_name"
+    t.string "middle_name"
+    t.string "last_name"
+    t.string "username"
+    t.integer "age"
+    t.string "phone_number"
+    t.text "biography"
+    t.string "pronouns"
+    t.string "industry"
+    t.string "job_position"
+    t.string "location"
+    t.string "education"
+    t.boolean "business_role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "applications", "cover_letters"
+  add_foreign_key "applications", "jobs"
+  add_foreign_key "applications", "resumes"
+  add_foreign_key "applications", "users"
+  add_foreign_key "cover_letters", "users"
+  add_foreign_key "experiences", "users"
+  add_foreign_key "job_skills", "jobs"
+  add_foreign_key "job_skills", "skills"
+  add_foreign_key "jobs", "users"
+  add_foreign_key "resumes", "users"
+  add_foreign_key "skills", "users"
+  add_foreign_key "user_skills", "skills"
+  add_foreign_key "user_skills", "users"
 end
